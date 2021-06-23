b4f5c1a7b742f8e4b8dff3b5d1040e55621136f1
EDA NUR YASDIMAN <enyasdiman@gmail.com>
2021-06-23 18:05:36 +0300

    Update Euler.m

--- Euler.m
+++ Euler.m
@@ -1,5 +1,5 @@
 %Euler Method
 clear all; clc; x0 = input('x(0): '); y0 = input('y(0): ');
 h = input('Number of iterations: '); n = input('Number of values: ');
-x = x0; y = y0; fprintf('\nResults (x,y)/n'); fprintf('%f\t%f\n', x, y);
+x = x0; y = y0; fprintf('\nResults (x,y)\n'); fprintf('%f\t%f\n', x, y);
 for i = 1:n y = y+h*(-y/2+x)); x = x+h; fprintf('%f\t%f\n', x, y); end
